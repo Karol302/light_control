139c94456c3a3ade88f92c2bc217ccc9a8e15799
Karol302 <56153402+Karol302@users.noreply.github.com>
2021-12-14 22:49:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python //if everything is ok, that will return a lux reading from the sensor
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-from tsl2561 import TSL2561
+from bh1750 import BH1750
 if __name__ == "__main__": 
-   tsl = TSL2561(debug=True)
+   tsl = BH1750(debug=True)
    print(tsl.lux())
